cbd810a72852a2308c0fe4e3b569e56772b79130
Tom <elunico@users.noreply.github.com>
2022-01-28 18:30:18 -0500

    Initial Commit

---
+++ iMacColorUnlocker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iMacColorUnlocker
+//
+//  Created by Thomas Povinelli on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iMacColorUnlocker/iMacColorUnlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMacColorUnlockerApp.swift
+//  iMacColorUnlocker
+//
+//  Created by Thomas Povinelli on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct iMacColorUnlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
